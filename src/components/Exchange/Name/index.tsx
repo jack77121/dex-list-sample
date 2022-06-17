adfd11cfd0efb112643454cf03d4d19a527f0147
Jack Lee <jack77121@gmail.com>
2022-06-17 15:45:35 +0800

    revise layout

--- src/components/Exchange/Name/index.tsx
+++ src/components/Exchange/Name/index.tsx
@@ -4,8 +4,6 @@ import { primary } from '../../../config/palette';
 const Name = ({ type = 'list', name = '', imgUrl = '' }) => {
   return (
     <Layout>
-      <Title>EX</Title>
-      <Gap type={type} />
       <Info>
         <img style={{ width: 32, height: 32 }} src={imgUrl} alt={`${name}-icon`} />
         <ExName style={{ marginLeft: 6 }}>{name}</ExName>
@@ -27,19 +25,9 @@ const Layout = styled.div`
   margin: 0px 8px;
 `;
 
-const Title = styled.div`
-  color: ${primary.color};
-  font-size: 18px;
-`;
-
-const Gap = styled.div<IName>`
-  width: 16px;
-  flex-grow: ${(props) => (props.type === 'list' ? 1 : 0)};
-`;
-
 const Info = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
 `;
 
