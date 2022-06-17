75103236f179aa3ff1bc0da5cce3a97a8f0836e3
Jack Lee <jack77121@gmail.com>
2022-06-17 18:01:54 +0800

    remove useless pkg

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import 'antd/dist/antd.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
